3cf71ef6227573264f84911994a1505c385d0313
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-14 09:34:56 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,148 @@
+// Today we practice to clear our concept about "==,===,!==,!=, <,>,<=,>=,and if else statement  
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+// var myName = prompt('Enter name');
+// var name = 'aziz';
+// console.log(myName == name);
+// if user enter myName same like i have stored in variable name than answer 
+// will come true otherwise false in console
+// we can under stand  == in console direct also without creating variables 
+'aziz' == 'aziz' 
+// console show true if we 
+'aziz' == 'azizullah' 
+// console answers false 
+
+// ------------------ mini-practice of(===) project-02 -------------------------- 
+// var num = prompt('Ente number');
+// var number = 24;
+// console.log(num === number); 
+// *********** if you enter 24 in input the answer shows false , and 24 is equal to 
+//   why----? 
+// var num = prompt('Ente number');
+// var number = 24;
+// console.log(num == number); 
+// now answer shows true, why---? you know that there is a difference b/w == and === 
+// == compares only value of two variable but === not compare only value but data type also 
+// you know that by prompt user alway give us value in string than first we should convert in first 
+// logic to number 
+//  var num = +prompt('Ente number');
+// var number = 24;
+// console.log(num === number); 
+//  now answer show true 
+// ------------------ mini-practice of(!=) project-03 -------------------------- 
+//  var color = prompt('Enter color name');
+//  var myFavouriteColor = 'blue';
+//  console.log(color != myFavouriteColor); 
+//  if user enter color in prompt green console show us 
+// true if user enter color in prompt blue console shows us false 
+
+// ------------------ mini-practice of(!==) project-04 -------------------------- 
+// var number = prompt('Enter number');
+// var newNum = 9;
+// console.log(number !== newNum); 
+// console answers true why...? 
+// see more 
+// var number = prompt('Enter number');
+// var newNum = 9;
+// console.log(number != newNum); 
+// console tells us false , why----- !
+// (!==) have same prform its duty like (===) and you know that (===) checks 
+// two conditions value and data type,similary  here !== is checking value and data type 
+//  in first, logic !== checks the value value we put 9 is equal to newNum now !== checks 
+// data type , that is not equal to newNum data type so console syas true 
+
+// ------------------ mini-practice of(>,<,>=,<=) project-05 -------------------------- 
+    // var num = +prompt('Enter number'); 
+    // var number = 6;
+    // console.log(num > 6); 
+// ==== console replies true if we enter number greater than 6; but there is a most 
+// ==== important point of clear concept about >,<, >=, <= ,first run your brain what i am 
+// saying! 
+//  you know that by pompt we always receive any number in string type now user enter 8 that is 
+//  string and our logic (num > 6) , is proves wrons because the user input number 7 is string 
+// than why console says true.....? 
+// ******* Remember our comparison operators (>,<,>=,<=) do not care for data type 
+// they check only value so i was trying to clear your concept and i hope you have understood 
+
+// ------------------ mini-practice of(<) project-06 -------------------------- 
+
+                // var num = prompt('Enter any number');
+                // var number = 5;
+                // console.log(num < 5);
+// === if user enter num smaller than 5 console replies true if user enter 
+//   enter num 5 or greater than 5 console replies false
+
+// ------------------ mini-practice of(>=) project-07 -------------------------- 
+                        // var num = prompt('Enter any number');
+                        // var number = 8;
+                        // console.log(num >= number); 
+// if user enter 8 or greater than 8 consoles show true if  use enter number less than 
+//  8 console replies false 
+
+// ------------------ mini-practice of(<=) project-08 -------------------------- 
+        // var num = prompt('Enter any number');
+        // var number = 20;
+        // console.log(num <= number);
+// if user enter number 20 or less than 20 console replies true, if user enter number greater than 
+//  20 conosle replies false;
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
+
+
+
+
+// ------------------ mini-practice of(==) project-01 -------------------------- 
